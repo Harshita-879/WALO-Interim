23d26b58f5d36dba161180a1703564f31d097913
atharva <atharvak.8023@gmail.com>
2024-04-21 16:09:51 +0530

    footer and hero4 changes

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,31 +3,28 @@ import waloFooter from '@/assets/walofooter.svg';
 import {Quicksand} from 'next/font/google';
 
 const quicksand = Quicksand ({
-  subsets: ['latin'],
-  weight: ['400','500']
+ subsets: ['latin'],
+ weight: ['400','500']
 })
 
 const Footer = () => {
-  return (
+ return (
     <main className={quicksand.className}>
-    <div className="w-1440 bg-gradient-to-r from-[#D8D0EE] to-[#a796d4] sm:pb-4">
-      <div className="sm:pt-4 mt-12">
-        <div className="flex justify-between items-center">
-          <div className="flex pl-[5%]">
-            
-        <Image className="mr-[10%] md:block"
-            src={waloFooter}
-          alt="footerIcon"
-          // height={300}
-          // width={400}
-        />
-        </div>
-          <p className="ml-16 text-sm sm:text-2xl font-bold leading-9 mr-[5%] md:block" style={{ color: '#6251A3' }}>Driving Growth, One Business at a Time.</p>
+      <div className="w-1440 bg-gradient-to-r from-[#D8D0EE] to-[#a796d4] sm:pb-1">
+        <div className="sm:pt-2 mt-12">
+          <div className="flex justify-between items-center">
+            <div className="flex pl-[5%]">
+              <Image className="mr-[10%] md:block h-14"
+                 src={waloFooter}
+                alt="footerIcon"
+              />
+            </div>
+            <p className="ml-16 text-sm sm:text-2xl font-bold leading-9 mr-[5%] md:block" style={{ color: '#6251A3' }}>Driving Growth, One Business at a Time.</p>
+          </div>
         </div>
       </div>
-    </div>
     </main>
-  )
+ )
 };
 
 export default Footer;
--- components/Hero4/Hero4.tsx
+++ components/Hero4/Hero4.tsx
@@ -10,31 +10,27 @@ const quicksand = Quicksand ({
 export default function Hero4() {
   return (
     <main className={quicksand.className}>
-    <div className="bg-custom-purple text-white flex flex-col items-center lg:ml-64 rounded-xl justify-center h-80 lg:w-2/3 md:gap-10 gap-5 mb-8 sm:my-8 mx-4 lg:mx-0">
+    <div className="bg-custom-purple text-white flex flex-col items-center lg:ml-64 rounded-xl justify-center h-64 lg:w-2/3 md:gap-5 gap-5 mb-8 sm:my-8 mx-4 lg:mx-0">
       <div>
         <h1 className="mt-6 lg:mt-0 text-3xl font-medium">
           Let's get in touch
         </h1>
-      </div>
-      <div className="lg:hidden">
-        <p className="flex items-center justify-center text-center mx-10 text-lg">
-          We'd love to help improve your experience with WALO. Get in touch with
-          us to know more about how to get started and for a more personalised
-          experience
-        </p>
       </div>
       <div className="w-2/3 hidden lg:inline">
         <p className="flex items-center justify-center">
         We are here to support your business ambitions.
         </p>
         <p className="flex items-center justify-center text-center">
-        Reach out to us today to learn more about how WALO can help your venture thrive.
+        Reach out to us today to learn more about how WALO can help your venture
+        </p>
+        <p className="flex items-center justify-center text-center">
+        thrive.
         </p>
       </div>
       <div>
       <a href="https://forms.gle/nw8pX7A7mHnZ6u177" target="_blank">
         <button
-          className="bg-custom-green text-white py-4 px-8 font-bold text-lg rounded-full mb-8 md:mb-0"
+          className="bg-custom-green text-white py-2 px-8 font-bold text-lg rounded-full mb-8 md:mb-0"
         >
           Get in Touch
         </button>
